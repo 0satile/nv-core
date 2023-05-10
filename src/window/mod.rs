9d4866f5a32f99d5ee97bf1183bb0dc7c7fa74bd
0satile <md.7m51@gmail.com>
2023-05-10 16:58:08 +0300

    ig it works

--- src/window/mod.rs
+++ src/window/mod.rs
@@ -2,14 +2,31 @@ use webview_app::{app::App, app::AppSettings};
  
 pub fn create_instance(window_title: &str, window_navigation_url: &str, window_width: i32, window_height: i32)
 {
-    let app = App::new(
-        AppSettings { 
-            title : window_title.to_string(),
-            url   : window_navigation_url.to_string(), 
-            width : window_width,
-            height: window_height,
-            ..Default::default()
-        }
-    );
-    app.run();
+    #[cfg(debug_assertions)] {
+        let app = App::new(
+            AppSettings { 
+                title           : window_title.to_string(),
+                url             : window_navigation_url.to_string(),
+                enable_dev_tools: true,
+                width           : window_width,
+                height          : window_height,
+                ..Default::default()
+            }
+        );
+        app.run();
+    }
+
+    #[cfg(not(debug_assertions))] {
+        let app = App::new(
+            AppSettings { 
+                title           : window_title.to_string(),
+                url             : window_navigation_url.to_string(),
+                enable_dev_tools: false,
+                width           : window_width,
+                height          : window_height,
+                ..Default::default()
+            }
+        );
+        app.run();
+    }
 }
